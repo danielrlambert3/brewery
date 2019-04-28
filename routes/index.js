92f6850c962dda784f53b47dee6dfaf4eb839a54
Daniel Lambert <daniel.lambert@plushrepublic.com>
2019-04-28 12:09:29 -0500

    added login and register routes and pages

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,17 @@ router.get("/", function(req, res){
     res.render("./pages/index");
 });
 
+// AUTH Routes
+
+// show register form
+router.get("/register", function(req, res){
+    res.render("./pages/register");
+});
+
+// show login form
+router.get("/login", function(req, res){
+    res.render("./pages/login");
+});
+
 
 module.exports = router;
